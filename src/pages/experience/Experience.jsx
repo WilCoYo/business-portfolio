3572a4f75e05dae2c45b2e07986823ac814284a9
William Yonkin <wilcoyonkin@gmail.com>
2025-06-24 11:59:19 -0400

    finished up the experience page

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -3,40 +3,71 @@ import './Experience.css'
 
 import Navigation from '../../components/navigation/Navigation'
 
+import code_cademy from '../../assets/images/Codecademy-logo.png'
+import americorps from '../../assets/images/NCCC-logo.png'
+
 function Experience() {
   return (
-    <div className='experience'>
-        <ul className='codeNinjas cell'>
-            <h2>Code Ninjas</h2>
-            <h4>Code Instructor</h4>
-            <li>Guided learners to aid understanding and knowledge of technology and coding concepts</li>
-            <li>Taught JavaScript, Python, and C+ fundamentals using Impact, Microsoft MakeCode, and Unity</li>
-            <li>Customized and supplement curricular materials to ensure knowledge transfer and growth</li>
-            <li>Leveraged educator background to inspire learners’ confidence in technology, math, and science</li>
-        </ul>
+    <div className='experience-page'>
 
-        <ul className='apple cell'>
-            <h2>Apple</h2>
-            <h4>Technical Expert</h4>
-            <li>Interpreted customer concerns and delivered appropriate product diagnoses and solutions</li>
-            <li>Flexibly rotated through multiple technical specialties and skill sets with ease and confidence</li>
-            <li>Translated technical knowledge via Apple iOS hardware and software technician certification</li>
-            <li>Troubleshot device issues with customers and communicated appropriate solutions</li>
-        </ul>
+     
 
-        <ul className='pics cell'>
-            <h2>PICS ITECH</h2>
-            <h4>Client Account Manager</h4>
-            <li>Managed CRM account data and quickly resolved service tickets queue for 60+ clients</li>
-            <li>Collaborated with clients to understand their business objectives and technical requirements</li>
-            <li>Liaised between clients and technical teams, facilitating communication and understanding</li>
-            <li>Collaborated with internal and external partners to drive projects from onboarding through closure</li>
-        </ul>
+      <div className='experience'>
+          <ul className='codeNinjas cell'>
+              <h2>Code Ninjas</h2>
+              <h4>Code Instructor</h4>
+              <li>Guided learners to aid understanding and knowledge of technology and coding concepts</li>
+              <li>Taught JavaScript, Python, and C+ fundamentals using Impact, Microsoft MakeCode, and Unity</li>
+              <li>Customized and supplement curricular materials to ensure knowledge transfer and growth</li>
+              <li>Leveraged educator background to inspire learners’ confidence in technology, math, and science</li>
+          </ul>
 
+          <ul className='apple cell'>
+              <h2>Apple</h2>
+              <h4>Technical Expert</h4>
+              <li>Interpreted customer concerns and delivered appropriate product diagnoses and solutions</li>
+              <li>Flexibly rotated through multiple technical specialties and skill sets with ease and confidence</li>
+              <li>Translated technical knowledge via Apple iOS hardware and software technician certification</li>
+              <li>Troubleshot device issues with customers and communicated appropriate solutions</li>
+          </ul>
 
-        <Navigation />
+          <ul className='pics cell'>
+              <h2>PICS ITECH</h2>
+              <h4>Client Account Manager</h4>
+              <li>Managed CRM account data and quickly resolved service tickets queue for 60+ clients</li>
+              <li>Collaborated with clients to understand their business objectives and technical requirements</li>
+              <li>Liaised between clients and technical teams, facilitating communication and understanding</li>
+              <li>Collaborated with internal and external partners to drive projects from onboarding through closure</li>
+          </ul>
+
+          
+         
+          <Navigation />
+      </div>
+
+      <div className='education-skills'>
+        <div className='codecademy cell'>
+          <img src= {code_cademy} className='codecademy-logo'></img>
+          <ul>
+            <li>Full-Stack Engineer Professional Certification</li>
+            <li>Front End Engineer Professional Certification</li>
+          </ul>
+        </div>
+
+        <div className='americorps cell'>
+          <img src= {americorps} className='americorps-logo'></img>
+          <ul>
+            <li>Americorps National Civilian Corps (NCCC)</li>
+            <li>AmeriCorps City Year, Chicago</li>
+          </ul>
+        </div>
+
+        
+      </div>
+
+      <div className='nav-space'></div>
+
     </div>
-    
   )
 }
 
