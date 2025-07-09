63ed6753ff23b1c7db8b0fc0afe5f3b290197513
William Yonkin <wilcoyonkin@gmail.com>
2025-07-09 15:46:32 -0400

    fixed moble nav button for resume

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import './Navigation.css'
 import MENUICON from '../../assets/images/menuIcon.png'
 import CLOSEICON from '../../assets/images/closeIcon.png'
-import DOWNLOADICON from '../../assets/images/download_icon.svg'
+
 import resume from '../../assets/files/Resume.pdf'
 
 
@@ -57,12 +57,11 @@ function Navigation() {
               rel="noreferrer"
               download
             >
-            <p 
-              id='resume-word'
-            >
-              Resume
-            </p>
-            <img src={DOWNLOADICON} className="download-icon" alt='download icon'/>
+              <p 
+                id='resume-word'
+              >
+                Resume
+              </p>
             </a>
             </button>
       </ul>
@@ -88,7 +87,23 @@ function Navigation() {
           <li onClick={() => navigate('/employer')}>About me</li>
           <li onClick={() => navigate('/experience')}>Experience</li>
           <li onClick={() => navigate('/projects')}>Projects</li>
-          <li>Resume</li>
+          <button  
+            className='resume-btn'
+            >
+
+            <a 
+              href= {resume}
+              target='_blank'
+              rel="noreferrer"
+              download
+            >
+              <p 
+                id='resume-word'
+              >
+                Resume
+              </p>
+            </a>
+            </button>
         </ul>  
     </div>
 
